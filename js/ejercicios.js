31b604599649d75f8d827460910dc01c30b97f8a
LAPTOP-JA5654I3\Liza <lizamihhedko@gmail.com>
2021-04-22 11:41:04 +0200

    Nivel_1 - ejercicio 6

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,15 +1,9 @@
-//Ejercicio 5 
-let nota_examen = 4.6;
+//Ejercicio 6 
 
-if (nota_examen < 0 || nota_examen > 10){
-    alert('Nota fuera de rango')
-} else if (nota_examen > 5) {
-    alert(`Felicidades, has aprobado el examen con un ${nota_examen}`)
-} else if (nota_examen < 5){
-    alert(`Ooooh has suspendido el examen con un ${nota_examen}`)
-}else {
-    alert('Por favor, introduce una nota válida')
-}
-
+let phrase = 'Tinc un cotxe de color blau';
+let replaceBlue = phrase.replace('blau', 'verd');
+console.log(replaceBlue);
+let replaceO = phrase.replace(/o/g, 'u');
+console.log(replaceO);
 
 
